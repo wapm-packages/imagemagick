6b4bb34152fa88b55e4ed570ecccbb8684a0bbac
Syrus <me@syrusakbary.com>
2019-04-11 23:47:07 -0700

    Updated build to output with O2 to preserve exported names

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 
 export CPPFLAGS="-I/code/libpng -I/code/zlib -I/code/libjpeg -I/code/libtiff/libtiff"
 export LDFLAGS="-L/code/zlib -L/code/libpng -L/code/libpng/.libs -L/code/libjpeg -L/code/libtiff/libtiff"
-export CFLAGS="-O3"
+export CFLAGS="-O2 -g2"
 export CXXFLAGS="$CFLAGS"
 MAKE_FLAGS="-s BINARYEN_TRAP_MODE=clamp -s ALLOW_MEMORY_GROWTH=1"
 
